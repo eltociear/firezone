7d5c1a6704bbfffdd067134b548128365236986b
Brian Manifold <bmanifold@gmail.com>
2023-08-24 11:39:29 -0400

    Apply recommended PR change and update failing acceptance tests

--- elixir/apps/web/test/web/live/settings/identity_providers/openid_connect/connect_test.exs
+++ elixir/apps/web/test/web/live/settings/identity_providers/openid_connect/connect_test.exs
@@ -46,9 +46,7 @@ defmodule Web.Auth.Settings.IdentityProviders.OpenIDConnect.Connect do
         conn
         |> authorize_conn(identity)
         |> assign(:account, account)
-        |> get(
-          ~p"/#{account}/settings/identity_providers/openid_connect/#{provider_id}/redirect"
-        )
+        |> get(~p"/#{account}/settings/identity_providers/openid_connect/#{provider_id}/redirect")
 
       assert redirected_to(conn) == ~p"/#{account}/settings/identity_providers"
       assert flash(conn, :error) == "Provider does not exist."
